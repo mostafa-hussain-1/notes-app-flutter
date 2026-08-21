bedcddcf26eb735a4d49909497ceda0b7019cd12
Mostafa Hussain <20251700804@cis.asu.edu.eg>
2026-08-21 03:30:28 +0300

    add & configure data base

--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -1,7 +1,29 @@
-class NoteModel {
-  final String id = DateTime.now().toString();
-  final String title;
-  final String content;
+import 'package:hive/hive.dart';
 
-  NoteModel({required this.title, required this.content});
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel {
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  String title;
+  @HiveField(2)
+  String content;
+  @HiveField(3)
+  NoteType type = NoteType.ACTIVE;
+
+  NoteModel({String? id, required this.title, required this.content}): id = id ?? DateTime.now().toString();
+}
+
+@HiveType(typeId: 1)
+enum NoteType {
+  @HiveField(0)
+  ACTIVE,
+  
+  @HiveField(1)
+  ARCHIVED,
+  
+  @HiveField(2)
+  DELETED,
 }
